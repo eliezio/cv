00f8df21e26ba898ac771e0f96bff4425d89ef47
Eliezio Oliveira <eliezio@pm.me>
2025-03-16 21:22:16 +0000

    Initial revision

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,16 @@
 import org.asciidoctor.Asciidoctor
 import org.asciidoctor.Attributes
 import org.asciidoctor.Options
-import org.asciidoctor.jruby.AsciiDocDirectoryWalker
 import java.io.File
 
-fun main(args: Array<String>) {
+fun main() {
     val attributes = Attributes.builder()
         .attribute("pdf-themesdir", "src/main/asciidoc/themes")
         .attribute("pdf-theme", "cv.yml")
         .build()
     val asciidoctor = Asciidoctor.Factory.create();
     asciidoctor.convertFile(
-        File("src/main/asciidoc/poznachowski.adoc"),
+        File("src/main/asciidoc/eliezio.adoc"),
         Options.builder()
             .attributes(attributes)
             .backend("pdf")
